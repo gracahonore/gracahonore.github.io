94a168139c11ed65037faddd706c0efe58718e0f
gracahonore <marcialmbango35@hotmail.com>
2014-11-28 07:09:19 -0800

    Projto inicial
    website

---
+++ projeto_web/javascript/jquery.slidedrop.js
@@ -0,0 +1,30 @@
+$(function(){
+
+$(".slider").cycle({
+
+timeout: 5000,
+fx: "scrollDown"
+});
+});
+$(document).ready(function(){
+$(".link_scond").hide();
+$(".aberta").click(function(){
+$(this).toggleClass("active").next().slideToggle("slow");
+return false;
+});
+});
+
+$(
+function(){
+$(".mendrop li").hover(
+
+function(){
+$("ul",this).slideDown(700);
+},
+function(){
+$("ul",this).slideUp(700);
+}
+);
+
+}
+);
